2dc1950ccdf4a160338d94119a45b9dbdada5a05
vivek <modanwal319@gmail.com>
2024-07-31 22:34:55 +0530

    updated frontend url

--- app/server/index.js
+++ app/server/index.js
@@ -13,7 +13,8 @@ const app = express();
 app.use(express.json());
 
 const corsOptions = {
-  origin: "http://localhost:3000",
+  // origin: "http://localhost:3000",
+  origin: "https://vivekumar192-gmail-com-client.vercel.app",
   credentials: true,
 };
 
